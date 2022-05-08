6a2d2c4d049bec67850d6ccb44fdade5845e7a6c
ShashikaHarshana <shashika.18@cse.mrt.ac.lk>
2022-05-08 19:49:39 +0530

    database mini project

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/SQLiteExpenseManager.java
@@ -0,0 +1,39 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import android.content.Context;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.InMemoryAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.InMemoryTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.SQLiteAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.SQLiteTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.ui.MainActivity;
+
+public class SQLiteExpenseManager extends ExpenseManager{
+    private Context context;
+
+    public SQLiteExpenseManager(Context context) throws ExpenseManagerException {
+        this.context = context;
+        setup();
+    }
+
+    @Override
+    public void setup() {
+        AccountDAO sqliteAccountDAO = new SQLiteAccountDAO(this.context);
+        setAccountsDAO(sqliteAccountDAO);
+
+       // TransactionDAO sqliteTransactionDAO = new InMemoryTransactionDAO();
+
+       TransactionDAO sqliteTransactionDAO = new SQLiteTransactionDAO(this.context);
+       setTransactionsDAO(sqliteTransactionDAO);
+
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
+    }
+}
